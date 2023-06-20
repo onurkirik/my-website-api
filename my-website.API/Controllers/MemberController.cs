d8f06e60dede92af6af185f105c8106a0f3b03c8
Onur Kırık <kirikonurr@gmail.com>
2023-06-20 05:06:21 +0300

    last changes

---
+++ my-website.API/Controllers/MemberController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using my_website.Application.DTOS.UserDTOS;
+using my_website.Domain.Entities;
+
+namespace my_website.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public MemberController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager; _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> SignIn([FromBody] UserLoginDto request)
+        {
+            var hasUser = await _userManager.FindByEmailAsync(request.Email);
+
+            if (hasUser == null)
+            {
+                return null!;
+            }
+
+            await _userManager.UpdateSecurityStampAsync(hasUser);
+
+            var signInResult = await _signInManager.PasswordSignInAsync(hasUser, request.Password, request.RememberMe, true);
+
+            var cookieValue = "myCookieValue";
+            var cookieOptions = new CookieOptions
+            {
+                Expires = DateTimeOffset.Now.AddDays(7),
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.None
+            };
+            Response.Cookies.Append("MyWebsiteCookie", cookieValue, cookieOptions);
+
+            if (signInResult.Succeeded)
+            {
+                return Ok(new { cookie = cookieValue });
+            }
+
+            return BadRequest();
+        }
+    }
+}
