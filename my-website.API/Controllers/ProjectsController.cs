ca4ec942914f943b110389a9c0c17fccb0f39c2d
Onur Kırık <kirikonurr@gmail.com>
2023-07-10 21:07:53 +0300

    last changes

--- my-website.API/Controllers/ProjectsController.cs
+++ my-website.API/Controllers/ProjectsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using my_website.Application.Repositories.ProjectRepository;
 
 namespace my_website.API.Controllers
 {
@@ -7,7 +9,36 @@ namespace my_website.API.Controllers
     [ApiController]
     public class ProjectsController : ControllerBase
     {
-        public ProjectsController()
+        private readonly IProjectReadRepository _projectReadRepository;
+        private readonly IProjectWriteRepository _projectWriteRepository;
+        private readonly IMapper _mapper;
+        public ProjectsController(IProjectReadRepository projectReadRepository, IProjectWriteRepository projectWriteRepository, IMapper mapper)
+        {
+            _projectReadRepository = projectReadRepository;
+            _projectWriteRepository = projectWriteRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("get-all")]
+        public async Task<IActionResult> Get()
+        {
+            var data = _projectReadRepository.GetAll();
+            return Ok(data);
+        }
+
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var data = await _projectReadRepository.GetByIdAsync(id, false);
+            return Ok(data);
+        }
+
+        [HttpPost]
+        [Route("create-project")]
+        public async Task<IActionResult> Create()
         {
             
         }
---
+++ my-website.Application/DTOS/ProjectDTOS/ProjectCreateDTO.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using my_website.Application.DTOS.ArticlesDTOS;
+using my_website.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_website.Application.DTOS.ProjectDTOS
+{
+    public class ProjectCreateDTO
+    {
+    }
+
+    public class ProjectCreateMappingProfile : Profile
+    {
+        protected ProjectCreateMappingProfile()
+        {
+        }
+    }
+}
